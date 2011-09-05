7ce63debe8c179c364549f40972834c865cd6353
Francois Lebel <francoislebel@gmail.com>
2011-09-05 16:27:23 -0400

    Updated the ADMIN_MEDIA_PREFIX setting.

--- robinson_app/settings.py
+++ robinson_app/settings.py
@@ -62,7 +62,7 @@ MEDIA_URL = '/media/'
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
 # Examples: "http://foo.com/media/", "/media/".
-ADMIN_MEDIA_PREFIX = '/adminmedia/'
+ADMIN_MEDIA_PREFIX = '/media/admin/'
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = ''
