5aef72b7634152b1c38fe108bba8a6c4219d9afa
Francois Lebel <francoislebel@gmail.com>
2016-01-21 00:00:56 -0800

    Added missing setting.

--- robinson_app/settings.py
+++ robinson_app/settings.py
@@ -9,6 +9,8 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
+ALLOWED_HOSTS = ['']
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
